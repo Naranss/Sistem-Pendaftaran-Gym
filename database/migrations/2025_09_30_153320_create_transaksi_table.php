71e0c47c07e9837321ee0baf45b72100e7ba5002
Naranss <pmtrial11@gmail.com>
2025-09-30 22:39:59 +0700

    Creating database migrations

---
+++ database/migrations/2025_09_30_153320_create_transaksi_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->date('tanggal');
+            $table->foreignId('id_produk')->nullable()->constrained('keranjang')->onDelete('cascade');
+            $table->foreignId('id_kontrak')->nullable()->constrained('kontrak')->onDelete('cascade');
+            $table->string('membership')->nullable();
+            $table->integer('jumlah_produk')->nullable();
+            $table->integer('harga_produk')->nullable();
+            $table->integer('harga_kontrak')->nullable();
+            $table->integer('harga_membership')->nullable();
+            $table->string('metode_pembayaran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaksi');
+    }
+};
