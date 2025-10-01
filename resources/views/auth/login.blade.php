8dacd961561e8cad8d4dea4365dfceba58e685d8
Naranss <pmtrial11@gmail.com>
2025-10-01 15:45:15 +0700

    Editing login page to use language file

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,23 +17,23 @@
     {{-- Form Login --}}
     <form method="POST" action="{{ route('login-auth') }}" class="space-y-4">
         @csrf
-        <h2 class="text-white text-3xl font-bold text-center">Login</h2>
+        <h2 class="text-white text-3xl font-bold text-center">{{__('Login')}}</h2>
 
-        <input name="username" type="text" placeholder="Username"
+        <input name="username" type="text" placeholder="{{__('Username')}}"
             class="w-full px-4 py-2 rounded-lg bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-yellow-400">
 
-        <input name="password" type="password" placeholder="Password"
+        <input name="password" type="password" placeholder="{{__('Password')}}"
             class="w-full px-4 py-2 rounded-lg bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-yellow-400">
 
         <button type="submit"
             class="w-full py-2 bg-yellow-500 text-black font-bold rounded-lg hover:bg-yellow-600 transition duration-300">
-            Login
+            {{__('Login')}}
         </button>
 
         <div class="text-gray-300 mt-4 text-sm text-center space-y-1">
-            <a href="#" class="hover:underline">Forgot Password?</a><br>
-            Don’t have an account?
-            <a href="{{ route('register') }}" class="font-bold hover:underline text-yellow-400">Register Now</a>
+            <a href="#" class="hover:underline">{{__('Forgot Password?')}}</a><br>
+            {{__("Don't have an account?")}}
+            <a href="{{ route('register') }}" class="font-bold hover:underline text-yellow-400">{{__('Register Now')}}</a>
         </div>
     </form>
 
